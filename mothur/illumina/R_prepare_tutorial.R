bbeeed246d9f95e81aa570d3cc4c6ab7aa491f24
Daniel Vaulot <vaulot@gmail.com>
2018-01-05 16:45:47 +0100

    fastq files

---
+++ mothur/illumina/R_prepare_tutorial.R
@@ -0,0 +1,6 @@
+source("C:/Users/vaulot/Google Drive/Scripts/R library/dv_function_ngs.R")
+
+
+# Subsample the fastq files
+
+fastq_subsample("C:/Data Biomol/RNA/Tags/CARBOM/fastq/tutorial", n_seq=1000) 
